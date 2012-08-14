601bc28ae25df044492aee95f757bdaffabc60e2
Jonathan Beilin <jbeilin@gmail.com>
2012-08-13 18:08:30 -0700

    Example project changes

---
+++ Example/DIYHeaderMenu/DIYViewController.h
@@ -0,0 +1,15 @@
+//
+//  DIYViewController.h
+//  DIYHeaderMenu
+//
+//  Created by Jonathan Beilin on 8/13/12.
+//  Copyright (c) 2012 DIY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DIYViewController : UIViewController
+
+@property (retain, nonatomic) IBOutlet UIButton *menuButton;
+
+@end
---
+++ Example/DIYHeaderMenu/DIYViewController.m
@@ -0,0 +1,48 @@
+//
+//  DIYViewController.m
+//  DIYHeaderMenu
+//
+//  Created by Jonathan Beilin on 8/13/12.
+//  Copyright (c) 2012 DIY. All rights reserved.
+//
+
+#import "DIYViewController.h"
+
+@interface DIYViewController ()
+
+@end
+
+@implementation DIYViewController
+
+@synthesize menuButton;
+
+#pragma mark - Init
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+#pragma mark - UI
+
+
+
+
+#pragma mark - Rotation
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return true;
+}
+
+#pragma mark - Dealloc
+
+- (void)dealloc {
+    [menuButton release];
+    [super dealloc];
+}
+@end
