5f1c504170b67d6dbc779c8ae94936174cf66e06
Jonathan Beilin <jbeilin@gmail.com>
2012-08-14 13:31:47 -0700

    Added DIYHeaderOptions

---
+++ DIYHeaderMenu/DIYHeaderOptions.h
@@ -0,0 +1,14 @@
+//
+//  DIYHeaderOptions.h
+//  DIYHeaderMenu
+//
+//  Created by Jonathan Beilin on 8/14/12.
+//  Copyright (c) 2012 DIY. All rights reserved.
+//
+
+#ifndef DIYHeaderMenu_DIYHeaderOptions_h
+#define DIYHeaderMenu_DIYHeaderOptions_h
+
+#define ITEMFRAME CGRectMake(0,0,140,50)
+
+#endif
